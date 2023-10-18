6f46fbc21b1884e2a14b6f45f395a6862f72826e
HjsCS <127371829+HjsCS@users.noreply.github.com>
2023-10-18 18:19:16 +1100

    fix: scss and svgr error

--- src/components/InfoPage/Author/index.tsx
+++ src/components/InfoPage/Author/index.tsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import style from './index.module.scss'
-import Star from '@/assets/InfoPageStar.svg'
+import React from "react";
+import style from "./index.module.scss";
+import Star from "../../../assets/InfoPageStar.svg";
 
-const Author
- = () => {
+const Author = () => {
   return (
     <div className={style.author_box}>
       <p className={style.author_name}>Mischca Sullivan</p>
-            <div className={style.stars}>
-                <Star />
-                <Star />
-                <Star />
-                <Star />
-                <Star />
-            </div>
+      <div className={style.stars}>
+        <Star />
+        <Star />
+        <Star />
+        <Star />
+        <Star />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Author
+export default Author;
