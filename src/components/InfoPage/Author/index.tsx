266b5cdfc30bde48beba5067f5a872b906773524
HjsCS <127371829+HjsCS@users.noreply.github.com>
2023-10-19 19:05:36 +1100

    fix: remove testing-library__jest-dom

--- src/components/InfoPage/Author/index.tsx
+++ src/components/InfoPage/Author/index.tsx
@@ -5,7 +5,7 @@ import Star from "@/assets/InfoPageStar.svg";
 const Author = (props: any) => {
   return (
     <div className={style.author_box} {...props}>
-      ß<p className={style.author_name}>Mischca Sullivan</p>
+      <p className={style.author_name}>Mischca Sullivan</p>
       <div className={style.stars}>
         <Star data-testid="star-icon" />
         <Star data-testid="star-icon" />
